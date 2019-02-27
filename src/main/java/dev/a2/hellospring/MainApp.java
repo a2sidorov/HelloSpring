49f7f2f1b2a507999fb8a49eb55ac38549ba2449
a2 <a2@acer-pc>
2019-02-27 17:44:57 +0300

    acer

--- src/main/java/dev/a2/hellospring/MainApp.java
+++ src/main/java/dev/a2/hellospring/MainApp.java
@@ -14,6 +14,11 @@ public class MainApp {
 	    obj.getMessage();
 	    obj.getMessage();
 	    
+	    //for loop from acer
+	    for (int i =0; i < 10; i++) {
+	    	System.out.println("hello");
+	    }
+	    
 	    
 	   }
 
