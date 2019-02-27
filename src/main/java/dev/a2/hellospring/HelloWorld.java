0d9845561baf1f6675d75ebf78bc50a98deb4336
a2 <a2@acer-pc>
2019-02-27 15:41:29 +0300

    Initial Commit

---
+++ src/main/java/dev/a2/hellospring/HelloWorld.java
@@ -0,0 +1,13 @@
+package dev.a2.hellospring;
+
+public class HelloWorld {
+	 private String message;
+
+	   public void setMessage(String message){
+	      this.message  = message;
+	   }
+	   public void getMessage(){
+	      System.out.println("Your Message : " + message);
+	   }
+
+}
