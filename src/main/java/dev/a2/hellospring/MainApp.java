b723f3c83a5bd2df96b7a1464f4ef1fdd539cd55
justi5ed <a2sidorov@gmail.com>
2019-02-27 17:03:35 +0300

    Test

--- src/main/java/dev/a2/hellospring/MainApp.java
+++ src/main/java/dev/a2/hellospring/MainApp.java
@@ -6,9 +6,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
 	
 	public static void main(String[] args) {
-	      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-	      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-	      obj.getMessage();
+	    //test
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+	    HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+	    obj.getMessage();
 	   }
 
 }
